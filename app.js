455e668aae378edb7cdc810e513c2d514d676c18
Sourav <thakursourav778@gmail.com>
2025-11-21 18:48:07 +0530

    image cursor wishlist

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-let main = document.querySelector('main')
-let crsr = document.querySelector('.cursor')
+let elem = document.querySelectorAll('.elem')
+let img = document.querySelector('.elem img')
 
-main.addEventListener('mousemove',(dets)=>{
-    crsr.style.left = dets.x+'px'
-    crsr.style.top = dets.y+'px'
-    
+
+elem.forEach((e)=>{
+  e.addEventListener('mouseenter',()=>{
+    e.children[1].style.opacity = '1'
+  })
+  e.addEventListener('mouseleave',()=>{
+    e.children[1].style.opacity = '0'
+  })
+
+  e.addEventListener('mousemove',(dets)=>{
+  e.children[1].style.left = dets.x+"px"
+  e.children[1].style.top = dets.y+"px"
+})
+  
 })
