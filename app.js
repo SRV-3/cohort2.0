07fa10c9ce5b174fd83ff8869fa6045a46bec073
Sourav <thakursourav778@gmail.com>
2025-11-21 13:15:39 +0530

    custom cursor

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-let h5 = document.querySelector('h5');
-let add = document.querySelector('.add');
-let flag = 0;
-add.addEventListener('click',()=>{
-  if(flag==0){
-    h5.innerText = 'Friends'
-    h5.style.color = 'green'
-    add.innerHTML = 'Remove'
-    add.style.backgroundColor = 'red'
-    flag = 1
-  }else{
-    h5.innerText = 'Strangers'
-    h5.style.color = 'red'
-    add.innerHTML = 'Add Friend'
-    add.style.backgroundColor = 'royalblue'
-    flag = 0
-  }
+let main = document.querySelector('main')
+let crsr = document.querySelector('.cursor')
+
+main.addEventListener('mousemove',(dets)=>{
+    crsr.style.left = dets.x+'px'
+    crsr.style.top = dets.y+'px'
+    
 })
